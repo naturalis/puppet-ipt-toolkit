b2f2d5d7e4eb170d2ec8b619a3c8d78937775764
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2013-10-28 16:46:28 +0100

    init ipt module

--- manifests/init.pp
+++ manifests/init.pp
@@ -35,7 +35,42 @@
 #
 # Copyright 2013 Your name here, unless otherwise noted.
 #
-class ipt-toolkit {
+class ipt-toolkit (
+  $datadir 		= '/data/ipt',
+  $datarootdirs 	= ['/data'],
+  $iptsource 		= 'https://gbif-providertoolkit.googlecode.com/files/ipt-2.0.5-security-update-1.war',
+  $iptname		= 'ipt-2.0.5-security-update-1',  
+  $deployroot		= '/var/lib/tomcat6/webapps',
+  )
+{
+  file { $datarootdirs:
+    ensure         => 'directory',
+    mode           => '0755',
+  }
 
+  file { $datadir:
+    ensure         => 'directory',
+    mode           => '0777',
+    require	   => File[$datarootdirs],
+  }
+  class { "tomcat": 
+    puppi    => true,
+  }
 
+  puppi::project::war { "ipt-toolkit":
+    source           => $iptsource,
+    deploy_root      => $deployroot,
+    enable	     => true,
+    auto_deploy	     => true,
+    always_deploy    => true,
+    clean_deploy     => true,
+    require	     => Class['tomcat'],
+
+  }
+
+  file { "${deployroot}/${iptname}/datadir.location":
+    content 	     => template('ipt-toolkit/datadir.location.erb'),
+    mode             => '0600',
+    require          => Exec['Run_Puppi_ipt-toolkit'],
+ }   
 }
