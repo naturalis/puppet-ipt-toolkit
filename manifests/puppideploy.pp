36707d70296de99c52b8ff0deb2f943be5d334d2
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2013-10-30 16:51:08 +0100

    moved puppi deploy to seperate class

---
+++ manifests/puppideploy.pp
@@ -0,0 +1,15 @@
+#
+# == Class: ipt-toolkit::proxy
+#
+#
+class ipt-toolkit::puppideploy (
+  $source               = undef,
+  $deploy_root          = undef,
+)
+{
+
+puppi::project::war { "ipt-toolkit":
+    source           => $source,
+    deploy_root      => $deploy_root,
+  } 
+}
